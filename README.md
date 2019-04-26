1c29fedced3a9a7ed9a72ecb38fdee73a523fe71
Andreas <roflcoffel@gmail.com>
2019-04-26 12:07:00 +0200

    python version added

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-# lw_code_challenge
+# LW Code Challenge
 
 A code challenge from LearningWell
 
+## JavaScript
+
 demo at: https://roflcoffel.github.io/lw_code_challenge/
+
+## Python (/python)
+
+clone the project or just download the [main.py](python/main.py) file.
+
+        git clone https://github.com/Roflcoffel/lw_code_challenge.git
+
+install scb python wrapper (pip is included in the install from python version 3.4).
+
+        pip install pyscbwrapper
+
+run the program with:
+
+        python main.py
--- python/main.py
+++ python/main.py
@@ -1,14 +1,55 @@
 from pyscbwrapper import SCB
-import json
-import requests
 
 #https://api.scb.se/OV0104/v1/doris/sv/ssd/ME/ME0104/ME0104D/ME0104T4
 scb = SCB('en', 'ssd', 'ME', 'ME0104', 'ME0104D', 'ME0104T4')
 
-print(scb.get_variables())
+metatable = scb.get_variables()
 
-##set_query()
-##get_query()
-##get_data()
+m_region = metatable["region"]
+m_years  = metatable["election year"]
 
-##print(scb.info())
+scb.set_query(
+        region=m_region,
+        observations=["Voting rates in election to the Riksdag, percent"]
+)
+
+m_region_id = scb.get_query()["query"][0]["selection"]["values"]
+
+#Map id to region_name
+regions = {}
+for i in range(len(m_region_id)):
+        regions[m_region_id[i]] = m_region[i]
+
+datatables = scb.get_data()["data"]
+
+data = []
+for obj in datatables:
+        data.append({
+                "region_name":regions[obj["key"][0]],
+                "year":obj["key"][1],
+                "percentage":obj["values"][0]
+        })
+
+highest_per_year = []
+#Filter Per Year
+for year in m_years:
+        selYear = list(filter(lambda d : d["year"] == year, data))
+
+        #Find Highest Percentage
+        curHighest = selYear[0]
+        for obj in selYear:
+                if obj["percentage"] > curHighest["percentage"]:
+                        curHighest = obj
+                elif obj["percentage"] == curHighest["percentage"] and obj["region_name"] != curHighest["region_name"]:
+                        curHighest["region_name"] = curHighest["region_name"] + ", " + obj["region_name"]
+
+        highest_per_year.append(curHighest)
+
+
+#Output
+for data in highest_per_year:
+        print(data["year"] + " : " + data["region_name"] + " : " + data["percentage"] + "%")
+
+
+
+
